3cfd9fc8e4ba5eb9638e7e19f7e9bd910c242cbb
sisufuyu <sisu.fuyu@gmail.com>
2021-12-01 23:16:46 +0200

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
+import Detail from '@/pages/detail/Detail'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/city',
+    name: 'City',
+    component: City
+  },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: Detail
+  }
+]
+
+const router = new VueRouter({
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
+})
+
+export default router
