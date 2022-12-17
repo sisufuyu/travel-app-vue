e56d466430ec4415ed257ff08be2312b07279771
Yu Fu <sisu.fuyu@gmail.com>
2022-12-17 14:58:06 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://travel-app-yufu.netlify.app',
         pathRewrite: {
           '^/api': 'mock'
         }
