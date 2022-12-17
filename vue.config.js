ad7fb72bf0d5689cccd183e07d6ebbe8f3d695d4
Yu Fu <sisu.fuyu@gmail.com>
2022-12-17 15:06:58 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://travel-app-yufu.netlify.app',
-        pathRewrite: {
-          '^/api': 'mock'
-        }
-      }
-    }
-  },
   chainWebpack: config => {
     config.resolve.alias
       .set('styles', path.join(__dirname, './src/assets/styles/'))
