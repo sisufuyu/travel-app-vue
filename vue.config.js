07c7b07fd24be88bc4c48da7e7d6ab2e1e9aa2bb
sisu.fuyu <sisu.fuyu@gmail.com>
2022-12-18 08:32:10 +0200

    change vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 const path = require('path');
 
 module.exports = {
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8080',
+  //       pathRewrite: {
+  //         '^/api': 'mock'
+  //       }
+  //     }
+  //   }
+  // },
   chainWebpack: config => {
     config.resolve.alias
       .set('styles', path.join(__dirname, './src/assets/styles/'))
